cc12419e85a676c23c8332aed2aac7708af1e61e
Arimazz <bodyaprymyak@icloud.com>
2020-02-18 16:22:58 +0200

    Updates

---
+++ precache-manifest.902bd199b5c07909434c4e2aac7dd77b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf6cba21517e380b840e22390520e3b",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "63ee502bedb64b6785bf",
+    "url": "/react_list-of-goods/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "029df8ab1dedc289b5ac",
+    "url": "/react_list-of-goods/static/js/2.0e6309e2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_list-of-goods/static/js/2.0e6309e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63ee502bedb64b6785bf",
+    "url": "/react_list-of-goods/static/js/main.851906e7.chunk.js"
+  },
+  {
+    "revision": "e23f53a6071b4c8b85e3",
+    "url": "/react_list-of-goods/static/js/runtime-main.d8819651.js"
+  }
+]);
